4fa804cafef059113aa287740448f6f6b33dd109
FearCleevan <jonathan.mauring17@gmail.com>
2025-08-26 04:18:03 +0800

    updates

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
+
   return {
     plugins: [react()],
-    base: env.VITE_BASE_PATH || '/portfolio',
+    base: mode === 'admin' 
+      ? (env.VITE_BASE_ADMIN_PATH || '/LoginPanel') 
+      : (env.VITE_BASE_PATH || '/portfolio')
   }
 })
